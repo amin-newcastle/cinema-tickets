fdab9caf0959c9fb6f94c1b0aa8d840ab9e5aa42
Muhammad Karim <muhammad-karim@hotmail.com>
2023-05-15 14:03:17 +0100

    Implement calculation of total seats and seat reservation request

--- cinema-tickets-javascript/src/pairtest/TicketService.js
+++ cinema-tickets-javascript/src/pairtest/TicketService.js
@@ -1,11 +1,15 @@
 import TicketTypeRequest from './lib/TicketTypeRequest.js';
 import InvalidPurchaseException from './lib/InvalidPurchaseException.js';
 import TicketPaymentService from '../thirdparty/paymentgateway/TicketPaymentService.js';
+import SeatReservationService from '../thirdparty/seatbooking/SeatReservationService.js';
 
 export default class TicketService {
   /**
    * Should only have private methods other than the one below.
    */
+
+  paymentGateway = new TicketPaymentService();
+  seatBookingService = new SeatReservationService();
 
   purchaseTickets(accountId, ...ticketTypeRequests) {
     // Check if the purchase request exceeds the maximum number of tickets
@@ -43,6 +47,17 @@ export default class TicketService {
 
     // Make a payment request to the TicketPaymentService
     this.paymentGateway.makePayment(accountId, totalAmount);
+
+    // Calculate the total number of seats to reserve
+    let totalSeatsToReserve = 0;
+    for (const request of ticketTypeRequests) {
+      if (request.getTicketType() !== 'INFANT') {
+        totalSeatsToReserve += request.getNoOfTickets();
+      }
+    }
+
+    // Make a seat reservation request to the SeatReservationService
+    this.seatBookingService.reserveSeat(accountId, totalSeatsToReserve);
 
     // TODO: Implement the rest of the purchaseTickets method
   }
